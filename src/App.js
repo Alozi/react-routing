1e121633d33ebf632e3b0e3e29d17baa03507222
Alozi <alozimozi@gmail.com>
2025-05-13 17:26:31 +0300

    Add index route

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <Home /> },
+      { index: true, element: <Home /> },
       { path: 'products', element: <Products /> },
       { path: 'products/:productId', element: <ProductDetail /> }
     ]
